6659770d66945a525a77a54f9b82d9fd51b926dc
Chris Morrow <morrowc@ops-netman.net>
2024-09-03 04:23:06 +0100

    Initial server code commit.
    Comments and basic scaffolding start.

---
+++ main.go
@@ -0,0 +1,12 @@
+// Simple http server application with three endpoints:
+//
+//	/login - login to the server, set an auth cookie.
+//	/stuff - show a page only after authentication.
+//	/logout - destroy the auth cookie.
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("vim-go")
+}
